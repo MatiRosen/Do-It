e35f9135cc5d84be8a117f81a8c2173db26fd8da
Julia <valdojuli@hotmail.com>
2023-09-20 10:42:55 -0300

    Creacion del proyecto

---
+++ Do-It/build.gradle.kts
@@ -0,0 +1,14 @@
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val nav_version = "2.5.3"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+}
