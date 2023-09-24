87c576dc3db0b3d5ba4c79c5dca75c7477a5957b
MatiRosen <matias.rosenstein00@gmail.com>
2023-09-24 19:39:32 -0300

    Creadas LoginActivity, fragments y diseñado el activity_main correctamente

---
+++ Do-It/app/src/main/java/team/doit/do_it/activities/LoginActivity.kt
@@ -0,0 +1,26 @@
+package team.doit.do_it.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import team.doit.do_it.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val loginButton = findViewById<Button>(R.id.btnLogin)
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
