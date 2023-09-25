ffb33519b169040f8877ede0f8c528a7b829748a
MatiRosen <matias.rosenstein00@gmail.com>
2023-09-25 20:16:16 -0300

    Renombré cosas mal nombradas, cree la navegación entre pantallas, cree pantallas faltantes, cambie el estilo para que funcionen bien los botones

--- Do-It/app/src/main/java/team/doit/do_it/activities/MainActivity.kt
+++ Do-It/app/src/main/java/team/doit/do_it/activities/MainActivity.kt
@@ -2,11 +2,24 @@ package team.doit.do_it.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import team.doit.do_it.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavView : BottomNavigationView
+    private lateinit var navHostFragment : NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.main_host) as NavHostFragment
+
+        bottomNavView = findViewById(R.id.bottom_navigation_view)
+
+        NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
     }
 }
---
+++ Do-It/app/src/main/java/team/doit/do_it/fragments/ChatFragment.kt
@@ -0,0 +1,21 @@
+package team.doit.do_it.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import team.doit.do_it.R
+
+class ChatFragment : Fragment() {
+
+    private lateinit var v : View
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        v = inflater.inflate(R.layout.fragment_chat, container, false)
+
+        return v
+    }
+}
