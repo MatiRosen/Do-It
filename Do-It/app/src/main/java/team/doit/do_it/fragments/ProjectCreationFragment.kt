40d7f5dd88eb51dd56c4254bdfda1f4ea33f4459
MatiRosen <matias.rosenstein00@gmail.com>
2023-09-25 23:55:31 -0300

    Agregado el fragment de crear ideas

---
+++ Do-It/app/src/main/java/team/doit/do_it/fragments/ProjectCreationFragment.kt
@@ -0,0 +1,24 @@
+package team.doit.do_it.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import team.doit.do_it.R
+
+
+class ProjectCreationFragment : Fragment() {
+
+    private lateinit var v : View
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        v = inflater.inflate(R.layout.fragment_project_creation, container, false)
+
+        return v
+    }
+
+
+}
