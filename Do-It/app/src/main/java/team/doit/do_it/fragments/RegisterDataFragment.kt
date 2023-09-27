dfca7b5e3d06b4b4f7d328d63b44a47078ce6d5e
Julia <valdojuli@hotmail.com>
2023-09-26 21:54:00 -0300

    Creacion de usuarios en firebase

--- Do-It/app/src/main/java/team/doit/do_it/fragments/RegisterDataFragment.kt
+++ Do-It/app/src/main/java/team/doit/do_it/fragments/RegisterDataFragment.kt
@@ -7,10 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextView
 import team.doit.do_it.R
 class RegisterDataFragment : Fragment() {
 
     private lateinit var v : View
+
+    private lateinit var spinnerGender : Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,22 +25,36 @@ class RegisterDataFragment : Fragment() {
     ): View? {
         v = inflater.inflate(R.layout.fragment_register_data, container, false)
 
-        val spinnerGender: Spinner = v.findViewById(R.id.spn_register_data_gender)
-        ArrayAdapter.createFromResource(
-            v.context,
-            R.array.genders,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinnerGender.adapter = adapter
-        }
+        startSpinner()
 
         return v
     }
 
     override fun onStart() {
         super.onStart()
+    }
 
+    private fun startSpinner() {
+        spinnerGender = v.findViewById(R.id.spnRegisterDataGender)
+        val genders = resources.getStringArray(R.array.genders).toMutableList()
+        val hint = resources.getString(R.string.register_gender)
+        genders.add(0, hint)
 
+        val adapter = object : ArrayAdapter<String>(v.context, android.R.layout.simple_list_item_activated_1, genders) {
+            override fun isEnabled(position: Int): Boolean {
+                return position != 0
+            }
+
+            override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+                val view = super.getDropDownView(position, convertView, parent)
+
+                if (position == 0) {
+                    (view as TextView).setTextColor(resources.getColor(R.color.medium_gray, null))
+                    view.setBackgroundColor(0)
+                }
+                return view
+            }
+        }
+        spinnerGender.adapter = adapter
     }
 }
