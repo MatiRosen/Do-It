508c8d695597bb147f0eeee1e02c10f175420b3e
MatiRosen <matias.rosenstein00@gmail.com>
2023-10-03 21:58:06 -0300

    Arreglado los margenes del fragment project detail creator

---
+++ Do-It/app/src/main/java/team/doit/do_it/fragments/EditProjectFragment.kt
@@ -0,0 +1,30 @@
+package team.doit.do_it.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import team.doit.do_it.databinding.FragmentEditProjectBinding
+
+class EditProjectFragment : Fragment() {
+
+    private var _binding : FragmentEditProjectBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var v : View
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentEditProjectBinding.inflate(inflater, container, false)
+        v = binding.root
+        return v
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+    }
+}
--- Do-It/app/src/main/java/team/doit/do_it/fragments/ProjectDetailCreatorFragment.kt
+++ Do-It/app/src/main/java/team/doit/do_it/fragments/ProjectDetailCreatorFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.findNavController
 import team.doit.do_it.R
 import team.doit.do_it.databinding.FragmentProjectDetailCreatorBinding
@@ -25,7 +28,7 @@ class ProjectDetailCreatorFragment : Fragment() {
         v = binding.root
 
         hideBottomNav()
-
+        removeMargins()
 
         return v
     }
@@ -34,15 +37,25 @@ class ProjectDetailCreatorFragment : Fragment() {
         super.onStart()
 
         setTexts()
+        initializeButtons()
+    }
 
+    private fun initializeButtons() {
         binding.imgBtnProjectDetailCreatorBack.setOnClickListener {
             v.findNavController().navigateUp()
+        }
+
+        binding.imgBtnProjectDetailCreatorEdit.setOnClickListener {
+            val project = ProjectDetailCreatorFragmentArgs.fromBundle(requireArguments()).project
+            val action = ProjectDetailCreatorFragmentDirections.actionProjectDetailFragmentToEditProjectFragment(project)
+            v.findNavController().navigate(action)
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         showBottomNav()
+        showMargins()
     }
 
     private fun setTexts() {
@@ -57,6 +70,24 @@ class ProjectDetailCreatorFragment : Fragment() {
 
     private fun hideBottomNav() {
         requireActivity().findViewById<View>(R.id.bottomNavigationView).visibility = View.GONE
+    }
+
+    private fun removeMargins() {
+        requireActivity().findViewById<FragmentContainerView>(R.id.mainHost)
+            .layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+            )
+    }
+    private fun showMargins() {
+        val constraintSet = ConstraintSet()
+        constraintSet.connect(R.id.mainHost, ConstraintSet.TOP, R.id.guidelineMainActivityHorizontal3, ConstraintSet.BOTTOM)
+        constraintSet.connect(R.id.mainHost, ConstraintSet.BOTTOM, R.id.bottomNavigationView, ConstraintSet.TOP)
+        constraintSet.connect(R.id.mainHost, ConstraintSet.START, R.id.guidelineMainActivityVertical2, ConstraintSet.END)
+        constraintSet.connect(R.id.mainHost, ConstraintSet.END, R.id.guidelineMainActivityVertical98, ConstraintSet.START)
+
+
+        constraintSet.applyTo(requireActivity().findViewById(R.id.frameLayoutMainActivity))
     }
 
     private fun showBottomNav() {
