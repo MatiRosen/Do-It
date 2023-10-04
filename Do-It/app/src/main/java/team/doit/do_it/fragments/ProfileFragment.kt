fb43e2b53df836a0c7432c2794f56880a386364e
Arnold Amorin <aamorin.ar@gmail.com>
2023-10-04 17:43:45 -0300

    Correccion mal merge Arnold en el profileFragmentkt
    Correccion mal merge Arnold en el profileFragmentkt

--- Do-It/app/src/main/java/team/doit/do_it/fragments/ProfileFragment.kt
+++ Do-It/app/src/main/java/team/doit/do_it/fragments/ProfileFragment.kt
@@ -4,20 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import team.doit.do_it.databinding.FragmentProfileBinding
-import android.widget.TextView
-import com.google.firebase.firestore.FirebaseFirestore
-
+import com.google.firebase.firestore.*
+import team.doit.do_it.R
 
 class ProfileFragment : Fragment() {
 
     private var _binding : FragmentProfileBinding? = null
     private val binding get() = _binding!!
     private lateinit var v : View
-    private val db = FirebaseFirestore.getInstance()
-    private var creatorEmail : String? = null
+
+    interface OnUserFetchedListener {
+        fun onUserFetched(user: DocumentSnapshot?)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,41 +39,41 @@ class ProfileFragment : Fragment() {
     }
 
     private fun replaceData() {
-        val name = v.findViewById<TextView>(team.doit.do_it.R.id.txtProfileName)
-        val mail = v.findViewById<TextView>(team.doit.do_it.R.id.txtProfileEmail)
-        val phone = v.findViewById<TextView>(team.doit.do_it.R.id.txtProfilePhone)
-        val gender = v.findViewById<TextView>(team.doit.do_it.R.id.txtProfileGender)
-        val address = v.findViewById<TextView>(team.doit.do_it.R.id.txtProfileAddress)
+        val currentUser = FirebaseAuth.getInstance().currentUser
 
-        val user = FirebaseAuth.getInstance().currentUser
-        creatorEmail = ProfileFragmentArgs.fromBundle(requireArguments()).CreatorEmail
-
-        if(creatorEmail == " " ){
-            //TODO: replace data with user information
-            if (user != null) {
-                name.text = user.displayName.toString()
-                mail.text = user.email.toString()
-                phone.text = user.phoneNumber.toString()
-                gender.text = "Género"
-                address.text = "Dirección"
+        getUser(currentUser?.email.toString(), object : OnUserFetchedListener {
+            override fun onUserFetched(user: DocumentSnapshot?) {
+                if (user != null) {
+                    //TODO: add profile photo
+                    binding.txtProfileName.text = "${user.getString("nombre")} ${user.getString("apellido")}"
+                    binding.txtProfileEmail.text = user.getString("email")
+                    binding.txtProfilePhone.text = user.getString("telefono")
+                    binding.txtProfileGender.text = user.getString("genero")
+                    binding.txtProfileAddress.text = user.getString("direccion")
+                } else {
+                    Toast.makeText(activity, resources.getString(R.string.profile_dataUser_error), Toast.LENGTH_SHORT).show()
+                }
             }
-        }else{
-            val creatorUser =  db.collection("usuarios").whereEqualTo("email",creatorEmail)
-            creatorUser.get()
-                .addOnSuccessListener { querySnapshot ->
-                    for (document in querySnapshot) {
-                        name.text = document.getString("nombre")
-                        mail.text = document.getString("email")
-                        phone.text = document.getString("telefono")
-                        gender.text = document.getString("genero")
-                        address.text = document.getString("direccion")
-                    }
-                }
-                .addOnFailureListener { exception ->
-                    // Manejar errores en la consulta, si los hay
-                }
-
-        }
-
+        })
     }
+
+    private fun getUser(email: String, listener: OnUserFetchedListener) {
+        val db = FirebaseFirestore.getInstance()
+        val usersRef = db.collection("usuarios")
+
+        usersRef.whereEqualTo("email", email)
+            .get()
+            .addOnSuccessListener { documents ->
+                if (!documents.isEmpty) {
+                    val user = documents.documents[0]
+                    listener.onUserFetched(user)
+                } else {
+                    listener.onUserFetched(null)
+                }
+            }
+            .addOnFailureListener {
+                listener.onUserFetched(null)
+            }
+    }
+
 }
