14f2db1da9d35386f88c8d9618ad288b1413dbd4
Julia <valdojuli@hotmail.com>
2023-10-08 20:45:17 -0300

    Agrego fragment de terminos y condiciones

---
+++ Do-It/app/src/main/java/team/doit/do_it/fragments/TermsAndConditionsFragment.kt
@@ -0,0 +1,21 @@
+package team.doit.do_it.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import team.doit.do_it.R
+
+class TermsAndConditionsFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_terms_and_conditions, container, false)
+    }
+}
