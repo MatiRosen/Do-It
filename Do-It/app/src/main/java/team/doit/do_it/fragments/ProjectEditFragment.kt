f0f57a2fe2c333c1fa5257aa8b53da4cf3993f63
Francisco Lagares <60282514+Pancholag@users.noreply.github.com>
2023-10-25 12:39:13 -0300

    fixes edit project

--- Do-It/app/src/main/java/team/doit/do_it/fragments/ProjectEditFragment.kt
+++ Do-It/app/src/main/java/team/doit/do_it/fragments/ProjectEditFragment.kt
@@ -9,7 +9,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
@@ -35,7 +34,6 @@ class ProjectEditFragment : Fragment() {
 
     private lateinit var v : View
 
-    //TODO: actualizar mas campos, actualizar cuando se hace navigateup
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -43,6 +41,7 @@ class ProjectEditFragment : Fragment() {
         _binding = FragmentProjectEditBinding.inflate(inflater, container, false)
         v = binding.root
 
+        hideBottomNav()
         startSpinner()
 
         return v
@@ -76,6 +75,10 @@ class ProjectEditFragment : Fragment() {
         }
 
         replaceData()
+    }
+
+    private fun hideBottomNav() {
+        requireActivity().findViewById<View>(R.id.bottomNavigationView).visibility = View.GONE
     }
 
     private fun pickImage() {
@@ -175,27 +178,72 @@ class ProjectEditFragment : Fragment() {
                     }
             }
 
-            db.collection("ideas")
-                .whereEqualTo("creatorEmail", currentUser?.email)
-                .whereEqualTo("creationDate", ProjectEditFragmentArgs.fromBundle(requireArguments()).project.creationDate)
-                .get()
-                .addOnSuccessListener {querySnapshot ->
-                    for (document in querySnapshot.documents) {
-                        db.collection("ideas").document(document.id)
-                            .update(project)
-                            .addOnSuccessListener {
-                                Toast.makeText(activity, "Project updated", Toast.LENGTH_SHORT).show()
-                                v.findNavController().navigateUp()
-                            }
-                            .addOnFailureListener {
-                                Toast.makeText(activity, "Error updating project", Toast.LENGTH_SHORT).show()
-                            }
-                    }
-                }
+            if(validateFields(project)) {
 
-
+                    db.collection("ideas")
+                        .whereEqualTo("creatorEmail", currentUser?.email)
+                        .whereEqualTo("creationDate", ProjectEditFragmentArgs.fromBundle(requireArguments()).project.creationDate)
+                        .get()
+                        .addOnSuccessListener { querySnapshot ->
+                            for (document in querySnapshot.documents) {
+                                db.collection("ideas").document(document.id)
+                                    .update(project)
+                                    .addOnSuccessListener {
+                                        Toast.makeText(
+                                            activity,
+                                            resources.getString(R.string.project_edit_succeed),
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                        v.findNavController().navigateUp()
+                                    }
+                                    .addOnFailureListener {
+                                        Toast.makeText(
+                                            activity,
+                                            resources.getString(R.string.project_edit_failed),
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                            }
+                        }
+            }
         }
     }
+
+    private fun validateFields(project: HashMap<String, Any>) : Boolean{
+        val fieldsToCheck = listOf(
+            Pair(project["title"], resources.getString(R.string.project_edit_title_error)),
+            Pair(project["subtitle"], resources.getString(R.string.project_edit_subtitle_error)),
+            Pair(project["description"], resources.getString(R.string.project_edit_description_error)),
+            Pair(project["minBudget"], resources.getString(R.string.project_edit_min_budget_error)),
+            Pair(project["goal"], resources.getString(R.string.project_edit_goal_error))
+        )
+
+        for ((property, errorMessage) in fieldsToCheck){
+            if (property.toString().isEmpty()){
+                Snackbar.make(v, errorMessage, Snackbar.LENGTH_LONG).show()
+                return false
+            }
+        }
+
+        if (project["minBudget"].toString().toInt() < 0.0){
+            Snackbar.make(v, resources.getString(R.string.project_creation_min_budget_error), Snackbar.LENGTH_LONG).show()
+            return false
+        }
+
+        if (project["goal"].toString().toInt() <= 0.0){
+            Snackbar.make(v, resources.getString(R.string.project_creation_goal_error), Snackbar.LENGTH_LONG).show()
+            return false
+        }
+
+        if (project["goal"].toString().toInt() < project["minBudget"].toString().toInt()){
+            Snackbar.make(v, resources.getString(R.string.project_creation_min_budget_higher_than_goal_error), Snackbar.LENGTH_LONG).show()
+            return false
+        }
+
+
+        return true
+    }
+
 
     private fun startSpinner(){
         val categoryList = resources.getStringArray(R.array.categories_array).toMutableList()
@@ -203,22 +251,7 @@ class ProjectEditFragment : Fragment() {
         categoryList.remove(hint)
         categoryList.add(0, hint)
 
-        val adapter = object : ArrayAdapter<String>(v.context, android.R.layout.simple_list_item_activated_1, categoryList){
-            override fun isEnabled(position: Int): Boolean {
-                return position != 0
-            }
-
-            override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
-                val view = super.getDropDownView(position, convertView, parent)
-
-                if (position == 0) {
-                    (view as TextView).setTextColor(resources.getColor(R.color.medium_gray, null))
-                    view.setBackgroundColor(0)
-                }
-
-                return view
-            }
-        }
+        val adapter = object : ArrayAdapter<String>(v.context, android.R.layout.simple_list_item_activated_1, categoryList){}
 
         binding.spinnerProjectEditCategory.adapter = adapter
     }
